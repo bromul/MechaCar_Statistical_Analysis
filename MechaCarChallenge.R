c3b1f2f38c737aaf05d58465a0024f9bcaef5da5
bromul <mtthw.mldn@gmail.com>
2021-06-26 15:56:42 -0400

    Deliverable 2 added

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,17 +2,33 @@
 library(dplyr)
 
 
+## Deliverable 1 Technical Analysis
+
 # Read in MechaCar_mpg.csv data
 MECHA_MPG <- read.csv(file = 'Data/MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
-# head(MECHA_MPG)
+head(MECHA_MPG)
 
 # Linear Regression to predict MPG
-mpg_model <- lm(mpg ~ ., data = MECHA_MPG)
-mpg_model 
+mpg_model <- lm(mpg ~ ., data = MECHA_MPG) 
+
 
 # Summarize mpg model
 summary(mpg_model)
-# R^2: .7149
-# Adj R^2: .6825
-# P: 5.35.-11
 
+
+
+## Deliverable 2 Technical Analysis
+
+# Read in Suspension_Coil.csv data
+COIL_DATA <- read.csv(file = 'Data/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+head(COIL_DATA)
+
+# Create total_summary df of coils' PSI
+total_summary <- COIL_DATA %>% 
+   summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+
+# Create lot_summary 
+lot_summary <- COIL_DATA %>% 
+   group_by(Manufacturing_Lot) %>% 
+   summarise(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
